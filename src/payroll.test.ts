c5738b76fa42761ec40dff56eb6a4784bc5168b2
TheGamingSimon <simon.pichler@bluewin.ch>
2025-05-07 19:15:31 +0200

    Produktiv Code für 18 jähriger success

--- src/payroll.test.ts
+++ src/payroll.test.ts
@@ -14,13 +14,45 @@ test("16 Jähriger mit 700 Fr. Monatslohn, Deductions: ALV(7.7 Fr.) & NBU(5.11 F
     expect(payslip.deductions.get("EO")).toBe(0);
     expect(payslip.deductions.get("PK")).toBe(0);
 
-    const alv = 700*0.011;
-    const nbu = 700*0.0073;
+    const alv = salary.gross*0.011;
+    const nbu = salary.gross*0.0073;
     expect(payslip.deductions.get("ALV")).toBeCloseTo(alv,2);
     expect(payslip.deductions.get("NBU")).toBeCloseTo(nbu,2);
 
     const expectedDeductions=alv+nbu;
     expect(payslip.totalDeductions).toBeCloseTo(expectedDeductions,2);
 
-    expect(payslip.net).toBeCloseTo(700-expectedDeductions,2);
+    expect(payslip.net).toBeCloseTo(salary.gross-expectedDeductions,2);
+})
+
+test("18 Jähriger mit 1200 Fr. Monatslohn, Deductions: ALV, NBU, AHV, IV, EO", ()=>{
+    //Arrange
+    const salary: Salary = {
+        born: new Date(2007,6,12),
+        payday: new Date(2025,5,31),
+        gross: 1200
+    };
+
+    //Act
+    const payslip = calculatePayslip(salary);
+
+    //Assert
+    expect(payslip.deductions.get("PK")).toBe(0);
+
+    const alv = salary.gross*0.011;
+    const nbu = salary.gross*0.0073;
+    const ahv = salary.gross*0.087;
+    const iv = salary.gross*0.014;
+    const eo = salary.gross*0.005;
+
+    expect(payslip.deductions.get("ALV")).toBeCloseTo(alv,2);
+    expect(payslip.deductions.get("NBU")).toBeCloseTo(nbu,2);
+    expect(payslip.deductions.get("AHV")).toBeCloseTo(ahv, 2);
+    expect(payslip.deductions.get("IV")).toBeCloseTo(iv, 2);
+    expect(payslip.deductions.get("EO")).toBeCloseTo(eo, 2);
+
+    const expectedDeductions=alv+nbu+ahv+iv+eo;
+    expect(payslip.totalDeductions).toBeCloseTo(expectedDeductions,2);
+
+    expect(payslip.net).toBeCloseTo(salary.gross-expectedDeductions,2);
 })
--- src/payroll.ts
+++ src/payroll.ts
@@ -24,16 +24,28 @@ export type Payslip = {
 
 export function calculatePayslip(salary: Salary): Payslip {
   const deductions: Deductions = new Map();
-  deductions.set("AHV",0);
-  deductions.set("IV",0);
-  deductions.set("EO",0);
+  const age = new Date(salary.born.getFullYear() + 17, salary.born.getMonth(), salary.born.getDay());
+  if(salary.payday < age){
+    deductions.set("AHV",0);
+    deductions.set("IV",0);
+    deductions.set("EO",0);
+  }
+
+  else{
+    deductions.set("AHV",salary.gross*(DEDUCTION_RATES.get("AHV")/100));
+    deductions.set("IV",salary.gross*(DEDUCTION_RATES.get("IV")/100));
+    deductions.set("EO",salary.gross*(DEDUCTION_RATES.get("EO")/100));
+  }
+
   deductions.set("PK",0);
   deductions.set("ALV",salary.gross*(DEDUCTION_RATES.get("ALV")/100));
   deductions.set("NBU",salary.gross*(DEDUCTION_RATES.get("NBU")/100));
+
   var totalDeductions = 0;
   deductions.forEach(deduction => {
     totalDeductions += deduction;
   });
+  
   const result: Payslip = {
     salary: salary,
     deductions: deductions,
